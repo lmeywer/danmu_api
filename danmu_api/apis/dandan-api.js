1dff78e7f2d4e28b11676138ef0a2260d60e0c57
lmeywer <Z1321446986@gmail.com>
2025-12-07 10:41:19 +0800

    Update dandan-api.js

--- danmu_api/apis/dandan-api.js
+++ danmu_api/apis/dandan-api.js
@@ -680,7 +680,7 @@ export async function searchEpisodes(url) {
     }
   }
 
-  log("info", `Found ${resultAnimes} animes with filtered episodes`);
+  log("info", `Found ${resultAnimes.episodes[0]} animes with filtered episodes`);
 
   return jsonResponse({
     errorCode: 0,
